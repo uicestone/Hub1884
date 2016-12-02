a53faaf99fba317b5fbc73ede849806d9767c93c
Uice Lu <uicestone@gmail.com>
2016-12-02 13:27:43 +0800

    fix: 新闻列表图片

--- wp-content/themes/omega/category-news.php
+++ wp-content/themes/omega/category-news.php
@@ -22,28 +22,6 @@
 		<div class="col-md-9">
 		<?php while(have_posts()): the_post(); ?>
 		<article class="post post-showinfo">
-			<div class="post-media">
-			<div class="figure element-no-top element-normal-bottom image-filter-none image-filter-onhover fade-in text-center figcaption-middle normalwidth" data-os-animation="none" data-os-animation-delay="0s">
-				<a class="figure-image" href="<?php the_permalink(); ?>">
-					<?php the_post_thumbnail(); ?>
-					<div class="figure-overlay grid-overlay-0">
-					<div class="figure-overlay-container">
-						<span class="figure-icon">
-						<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100" preserveAspectRatio="none"><g>
-							<path display="inline" fill="none" stroke-width="1" stroke-miterlimit="10" d="M45.634,50.124
-								  c-0.939-0.939-2.425-0.939-3.364,0l-9.833,9.831c-0.938,0.938-0.938,2.426,0,3.364l5.175,5.175c0.939,0.938,2.426,0.938,3.364,0
-								  l9.833-9.832c0.937-0.938,0.937-2.426,0-3.362L45.634,50.124z"></path>
-							<path display="inline" fill="none" stroke-width="1" stroke-miterlimit="10" d="M64.261,31.495
-								  c-0.937-0.938-2.423-0.938-3.362,0l-9.831,9.832c-0.94,0.938-0.94,2.424,0,3.364l5.174,5.174c0.938,0.939,2.427,0.939,3.364,0
-								  l9.831-9.832c0.94-0.938,0.94-2.424,0-3.362L64.261,31.495z"></path>
-							<line display="inline" fill="none" stroke-width="1" stroke-miterlimit="10" x1="44.34" y1="56.591" x2="57.535" y2="43.396"></line></g>
-						</svg>
-						</span>
-					</div>
-					</div>
-				</a>
-			</div>
-			</div>
 			<header class="post-head">
 			<h2 class="post-title">
 				<a href="<?php the_permalink() ?>">
